4614b5d0f428ba570ea176892fa947cce78242f6
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-12-03 23:18:37 -0300

    feat (AppointerMember): refactor do service para incluir o SpringException

--- src/main/java/br/com/ifba/infrastructure/service/AppointerMemberService.java
+++ src/main/java/br/com/ifba/infrastructure/service/AppointerMemberService.java
@@ -1,6 +1,7 @@
 package br.com.ifba.infrastructure.service;
 
 import br.com.ifba.infrastructure.entity.AppointerMember;
+import br.com.ifba.infrastructure.exception.BusinessException;
 import br.com.ifba.infrastructure.repository.AppointerMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,31 +21,24 @@ public class AppointerMemberService {
 
     public AppointerMember findById(Long id) {
         return appointerMemberRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Registro de membro em cargo não encontrado com ID: " + id));
+                .orElseThrow(() -> new BusinessException("Registro de membro não encontrado com ID: " + id));
     }
 
     @Transactional
-    public AppointerMember save(AppointerMember appointerMember) {
-
-        if (appointerMember.getUser() == null || appointerMember.getUser().getId() == null) {
-            throw new RuntimeException("É obrigatório informar o Usuário para atribuir um cargo.");
+    public AppointerMember save(AppointerMember member) {
+        if (member.getUser() == null || member.getUser().getId() == null) {
+            throw new BusinessException("É obrigatório informar o Usuário.");
         }
-
-        if (appointerMember.getAppointer() == null || appointerMember.getAppointer().getId() == null) {
-            throw new RuntimeException("É obrigatório informar a Nominata (Gestão) para atribuir um cargo.");
+        if (member.getAppointer() == null || member.getAppointer().getId() == null) {
+            throw new BusinessException("É obrigatório informar a Nominata.");
         }
-
-        if (appointerMember.getRole() == null) {
-            throw new RuntimeException("É obrigatório informar qual o cargo (Role) do membro.");
-        }
-
-        return appointerMemberRepository.save(appointerMember);
+        return appointerMemberRepository.save(member);
     }
 
     @Transactional
     public void delete(Long id) {
         if (!appointerMemberRepository.existsById(id)) {
-            throw new RuntimeException("Registro não encontrado para exclusão.");
+            throw new BusinessException("Registro não encontrado para exclusão.");
         }
         appointerMemberRepository.deleteById(id);
     }
