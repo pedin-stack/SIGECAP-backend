82e85a0c87b2ee97bf2c28a855f5960153cd60af
Pedro Dourado Rocha <pdourado2006@gmail.com>
2025-12-19 16:07:45 -0300

    config: adicão de contexto web para o main + criação do cors.config

--- src/main/java/br/com/ifba/SigecapApplication.java
+++ src/main/java/br/com/ifba/SigecapApplication.java
@@ -2,8 +2,10 @@ package br.com.ifba;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SigecapApplication {
 
 	public static void main(String[] args) {
